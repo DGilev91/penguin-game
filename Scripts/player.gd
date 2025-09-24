60eb506276c240f39ce6f66f9d6549beaef55034
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-24 21:15:26 +0500

    zoom

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -29,6 +29,7 @@ signal level_finished()
 @onready var anchor: Node2D = $Anchor
 @onready var sprite_2d: Sprite2D = $Anchor/Sprite2D
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 
 var target_titl: float = 0.0
@@ -93,6 +94,10 @@ func _physics_process(delta: float) -> void:
 		
 	sprite_2d.rotation_degrees = lerp(sprite_2d.rotation_degrees, target_titl, 0.2)
 	anchor.scale = anchor.scale.lerp(Vector2.ONE, 0.05)
+	
+	
+	var y_offset_target: float = clamp(ray_cast_2d.get_collision_point().y - global_position.y, -16, 64)
+	camera_2d.offset.y = lerp(camera_2d.offset.y, y_offset_target, 0.01)
 	
 	var zoom_target_amount: float = clamp(max_zoom_amount - (velocity.x / 150), min_zoom_amount , max_zoom_amount)
 	var zoom_target: Vector2 = Vector2(zoom_target_amount, zoom_target_amount)
